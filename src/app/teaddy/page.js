af7a6a37c8f234d284c191cb3e645eedf578ed6b
crystalhtc <crystalhtc01@gmail.com>
2025-08-14 14:03:13 -0700

    update teaddy link to white paper

--- src/app/teaddy/page.js
+++ src/app/teaddy/page.js
@@ -283,10 +283,10 @@ export default function Remedify() {
               <div className={styles.nextProject}>
                 <h2>Next Project is...</h2>
                 <NextCard
-                  name="One Timeless Step"
-                  image="/oneTimelessStep-homepage.png"
-                  alt="One Timeless Step"
-                  link="/oneTimelessStep"
+                  name="REPAIR Framework White Paper"
+                  image="/whitePaper-homepage.png"
+                  alt="REPAIR Framework White Paper"
+                  link="/whitePaper"
                   button="Next Project"
                 />
               </div>
